f28166f6893b42fc0be8986f900baa83a61b4fff
MarynaNogtieva <marynanogtieva@yahoo.ca>
2017-08-04 08:59:01 -0400

    completed shortest_string exercise

--- exercises/shortest_string.rb
+++ exercises/shortest_string.rb
@@ -4,10 +4,21 @@
 # Prints:      Nothing
 
 def shortest_string(list)
-  # This is your job. :)
+
+  shortest_string = list.first
+    list.each do |word|
+      #instead of length we can use count as well
+      if(word.length < shortest_string.length)
+        shortest_string = word
+      end
+    end
+
+    return shortest_string
 end
 
 if __FILE__ == $PROGRAM_NAME
-  # I'd advise putting some sanity checks here.
-  # How else will you be sure your code does what you think it does?
+  p shortest_string(["Lorem ipsum","Ne ius reque denique complectitur", ""]) == ""
+  p shortest_string(["Et his soleat soluta","Ne ius ", ""]) == ""
+  p shortest_string(["ipsum","Ne", "t"]) == "t"
+  p shortest_string(["Ldsa"," ", "dfdsf"]) == " "
 end
